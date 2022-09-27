50709f66d74e61d4c511e5650d49d3b779381dfd
Tolgatunaacar <110130709+Tolgatunaacar@users.noreply.github.com>
2022-09-27 20:32:12 +0300

    Add files via upload

---
+++ SQL-4.sql
@@ -0,0 +1,18 @@
+--SELECT DISTINCT replacement_cost FROM film
+--order by (replacement_cost);
+
+
+-- SELECT COUNT(DISTINCT(replacement_cost)) FROM film;
+
+
+--SELECT title,rating FROM film
+--WHERE (title LIKE 'T%') and rating = 'G';
+
+--SELECT country from country
+--Where country LIKE '_____';
+
+
+--SELECT city FROM city
+--WHERE city ILIKE '%R';
+
+
