b9a618b6b4a86b44d879e759e8353706d84d245e
Tolgatunaacar <110130709+Tolgatunaacar@users.noreply.github.com>
2022-10-02 15:53:02 +0300

    Add files via upload

---
+++ SQL-8.sql
@@ -0,0 +1,16 @@
+--CREATE TABLE employee ( 
+--	id SERIAL PRIMARY KEY,
+--	name VARCHAR(50),
+--	birthday DATE,
+--	email VARCHAR(100)
+	
+--);
+
+--UPDATE employee
+--SET name = 'Ata'
+--WHERE id BETWEEN 1 AND 5 ;
+
+--DELETE FROM employee
+--where id BETWEEN 20 AND 25;
+
+--SELECT * FROM employee
