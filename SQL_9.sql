ece32dc4dafa26eda25ac9b24218feda399eb71c
Tolgatunaacar <110130709+Tolgatunaacar@users.noreply.github.com>
2022-10-03 13:21:32 +0300

    Add files via upload

---
+++ SQL_9.sql
@@ -0,0 +1,10 @@
+--SELECT city,country FROM city
+--INNER JOIN country ON country.country_id = city.country_id;
+
+
+--SELECT payment.payment_id,customer.first_name,customer.last_name FROM customer
+--INNER JOIN payment ON payment.customer_id = customer.customer_id;
+
+
+--SELECT rental_id,first_name,last_name FROM rental
+--INNER JOIN customer ON customer.customer_id = rental.customer_id;
