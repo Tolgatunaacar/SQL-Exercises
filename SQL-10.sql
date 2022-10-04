7f8cb440079b096d5d961de1d114ec021e0c8e69
Tolgatunaacar <110130709+Tolgatunaacar@users.noreply.github.com>
2022-10-04 13:35:36 +0300

    Add files via upload

---
+++ SQL-10.sql
@@ -0,0 +1,12 @@
+--SELECT city,country FROM city
+--LEFT JOIN country ON country.country_id = city.country_id;
+
+
+--SELECT *
+--FROM customer
+--RIGHT JOIN payment ON payment.customer_id = customer.customer_id;
+
+
+--SELECT *
+--FROM customer
+--FULL JOIN rental ON rental.customer_id = customer.customer_id;
